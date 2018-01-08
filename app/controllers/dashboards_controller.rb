01efd69cd1f37364d4ac65095bd7bf700a55934c
alvi2496 <alvi.utsab@gmail.com>
2018-01-08 17:22:31 +0600

    complete one page signup signin

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,11 @@
 class DashboardsController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   def show
-    @user = current_user
+    if user_signed_in?
+      @user = current_user
+    else
+      redirect_to new_content_path
+    end
   end
 end
